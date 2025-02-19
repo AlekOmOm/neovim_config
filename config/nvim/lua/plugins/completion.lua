24dd8fd391785ed4c7a9a32e1c84954291eddb73
AlekOmOm <alekellegard@protonmail.com>
2025-02-19 11:35:16 +0000

    feat: updated completion and LSP configurations for nvim 0.9+ compatibility

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -1,20 +1,67 @@
-# ~/.config/nvim/lua/plugins/completion.lua
-local cmp = require('cmp')
+#-- ~/.config/nvim/lua/plugins/completion.lua
 
-cmp.setup({
-    snippet = {
-        expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-        end,
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    }),
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
+local M = {}
+
+function M.setup()
+    local status_ok, cmp = pcall(require, 'cmp')
+    if not status_ok then
+        return
+    end
+
+    local luasnip_ok, luasnip = pcall(require, 'luasnip')
+    if not luasnip_ok then
+        return
+    end
+
+    cmp.setup({
+        snippet = {
+            expand = function(args)
+                luasnip.lsp_expand(args.body)
+            end,
+        },
+        mapping = {
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.abort(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            ['<Tab>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                elseif luasnip.expand_or_jumpable() then
+                    luasnip.expand_or_jump()
+                else
+                    fallback()
+                end
+            end, { 'i', 's' }),
+            ['<S-Tab>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                else
+                    fallback()
+                end
+            end, { 'i', 's' }),
+        },
+        sources = cmp.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'luasnip' },
+            { name = 'buffer' },
+            { name = 'path' },
+        }),
+        formatting = {
+            format = function(entry, vim_item)
+                vim_item.menu = ({
+                    nvim_lsp = "[LSP]",
+                    luasnip = "[Snippet]",
+                    buffer = "[Buffer]",
+                    path = "[Path]",
+                })[entry.source.name]
+                return vim_item
+            end
+        },
     })
-})
+end
+
+return M 
--- config/nvim/lua/plugins/lsp/mason.lua
+++ config/nvim/lua/plugins/lsp/mason.lua
@@ -1,30 +1,98 @@
-# ~/.config/nvim/lua/plugins/lsp/mason.lua
+-- Enhanced mason configuration with better organization and additional features
+local status_ok, mason = pcall(require, "mason")
+if not status_ok then
+    return
+end
 
-require("mason").setup({
-    ensure_installed = {
-        "actionlint",
-        "prettier",
-        "markdownlint",
-        "markdown-toc"
-    }
-    
-    -- required fields Mason Settings 
+-- Core tool categories
+local FORMATTERS = {
+    "prettier",
+    "stylua",
+    "black",
+    "rustfmt",
+}
 
+local LINTERS = {
+    "actionlint",
+    "markdownlint",
+    "shellcheck",
+    "eslint_d",
+}
+
+local DOC_TOOLS = {
+    "markdown-toc",
+    "vale",
+}
+
+-- Language servers by domain
+local WEB_SERVERS = {
+    "html",
+    "cssls",
+    "tsserver",
+    "jsonls",
+}
+
+local SYSTEM_SERVERS = {
+    "dockerls",
+    "docker_compose_language_service",
+    "bashls",
+}
+
+local PROG_SERVERS = {
+    "pyright",
+    "rust_analyzer",
+    "lua_ls",
+}
+
+local CONFIG_SERVERS = {
+    "yamlls",
+    "vimls",
+}
+
+-- Base mason setup with extended configuration
+mason.setup({
+    ui = {
+        border = "rounded",
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    },
+    max_concurrent_installers = 4,
+    ensure_installed = vim.tbl_flatten({
+        FORMATTERS,
+        LINTERS,
+        DOC_TOOLS,
+    })
 })
 
+-- LSP configuration with grouped servers
 require('mason-lspconfig').setup({
-    ensure_installed = {
-        "pyright",
-        "dockerls",
-        "docker_compose_language_service",
-        "rust_analyzer",
-        "bashls",
-        "yamlls",
-        "vimls",
-        "jsonls",
-        "html",
-        "cssls",
-        "ts_ls",
-        "lua_ls"
-    }
+    ensure_installed = vim.tbl_flatten({
+        WEB_SERVERS,
+        SYSTEM_SERVERS,
+        PROG_SERVERS,
+        CONFIG_SERVERS,
+    }),
+    automatic_installation = true,
 })
+
+-- Auto-installation hook
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function()
+        local ft = vim.bo.filetype
+        -- Add automatic server installation logic here
+    end,
+})
+
+-- Optional: Integration with null-ls for formatting
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if null_ls_status_ok then
+    null_ls.setup({
+        sources = {
+            -- Add null-ls formatting sources here
+        }
+    })
+end
