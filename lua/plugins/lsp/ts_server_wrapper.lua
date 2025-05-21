ddc9c6cd1049d7176bca60514dc274670ceb92a6
Aleksander Hvid Ellegård <142972647+AlekOmOm@users.noreply.github.com>
2025-05-21 12:11:40 +0200

    Add docstring to TypeScript server wrapper

--- lua/plugins/lsp/ts_server_wrapper.lua
+++ lua/plugins/lsp/ts_server_wrapper.lua
@@ -1,4 +1,13 @@
 -- lua/plugins/lsp/ts_server_wrapper.lua
+--
+-- TypeScript LSP server wrapper to fix EventEmitter warnings
+--
+-- This module injects configuration to increase the EventEmitter default max listeners
+-- limit to prevent "MaxListenersExceededWarning" in Node.js TypeScript server.
+-- 
+-- Available options:
+--   config.max_listeners - Number of max listeners (default: 15)
+--
 local M = {}
 
 function M.setup(config)
