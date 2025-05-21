28cd4d9a6fe292ba71665e6a9d95030d42775241
Aleksander Hvid Ellegård <142972647+AlekOmOm@users.noreply.github.com>
2025-05-21 12:04:05 +0200

    Make EventEmitter max listeners configurable

--- lua/plugins/lsp/ts_server_wrapper.lua
+++ lua/plugins/lsp/ts_server_wrapper.lua
@@ -1,12 +1,15 @@
 -- lua/plugins/lsp/ts_server_wrapper.lua
 local M = {}
 
-function M.setup()
+function M.setup(config)
+  config = config or {}
+  local max_listeners = config.max_listeners or 15
+  
   -- Ensure the process.env exists before starting the server
-  local env_prepare = [[
+  local env_prepare = string.format([[
   process.setMaxListeners = process.setMaxListeners || function() {};
-  require('events').EventEmitter.defaultMaxListeners = 15;
-  ]]
+  require('events').EventEmitter.defaultMaxListeners = %d;
+  ]], max_listeners)
   
   return {
     before_init = function(params, config)
